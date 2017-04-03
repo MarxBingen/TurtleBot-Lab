da5aa3a756c46ecdfdf4b695a3f9f5604e1ad402
Daniel Kaiser <marx@th-bingen.de>
2017-04-03 18:44:33 +0200

    indentation fehler

--- src/tb_base/scripts/imuInfo.py
+++ src/tb_base/scripts/imuInfo.py
@@ -7,15 +7,15 @@ import math
 
 from sensor_msgs.msg import Imu
 
-	def imuCallback(data):
-		qx = data.orientation.x
-		qy = data.orientation.y
-		qz = data.orientation.z
-		qw = data.orientation.w
-		s3 = 2 * ((qw*qz) + (qx*qy))
-		s4 = 1 - 2 * ((qy*qy) + (qz*qz))
-		yaw = math.degrees(math.atan2(s3,s4))+180
-		print yaw
+def imuCallback(data):
+	qx = data.orientation.x
+	qy = data.orientation.y
+	qz = data.orientation.z
+	qw = data.orientation.w
+	s3 = 2 * ((qw*qz) + (qx*qy))
+	s4 = 1 - 2 * ((qy*qy) + (qz*qz))
+	yaw = math.degrees(math.atan2(s3,s4))+180
+	print yaw
 
 
 if __name__ == '__main__':
