041172056885760b9e3baa31e3587593beecdcfc
Daniel Kaiser <marx@th-bingen.de>
2017-05-09 12:33:22 +0200

    kleine aenderungen

--- src/tb_base/scripts/zufall.py
+++ src/tb_base/scripts/zufall.py
@@ -6,38 +6,28 @@ from TBRandom import zufall
 import time
 import cv2
 
-#from blinkstick import blinkstick
+from blinkstick import blinkstick
 
 if __name__ == '__main__':
 	base=TBBase(0.38)
-	#for bs in blinkstick.find_all():
-	#	bs.set_color(name='green')
+	for bs in blinkstick.find_all():
+		bs.set_color(name='green')
 	#base.vorwaerts()
 	#base.drehe() #
 	#time.sleep(2)
 	#base.drehe()
 	while not rospy.is_shutdown():
 		p = base.pruefeFelder()
-		v = []
-		print("Weg frei nach:"),
 		if p.rechts == 'Frei':
-			print("rechts "),
-			v.append('rechts')
-		if p.links == 'Frei':
-			print("links "),
-			v.append('links')
-		if p.mitte == 'Frei':
-			print("mitte "),
-			v.append('mitte')
-		print " "
-		z = zufall(v)
-		if (z=='mitte'):
+			base.drehe('rechts')
 			base.vorwaerts()
-		elif (z=='sackgasse'):
+		elif p.mitte == 'Frei':
+			base.vorwaerts()
+		elif p.links == 'Frei':
 			base.drehe('links')
-		else:
-			base.drehe(z)
 			base.vorwaerts()
+		else:
+			base.drehe('rechts')
 		time.sleep(0.5)
-	#for bs in blinkstick.find_all():
-		#bs.set_color(name='red')
+	for bs in blinkstick.find_all():
+		bs.set_color(name='red')
