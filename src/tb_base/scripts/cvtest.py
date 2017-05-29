0b52d3bf1a68a95526cbd44339e7e58c50f6dbdc
Daniel Kaiser <marx@th-bingen.de>
2017-05-29 13:31:10 +0200

    Kreuzerkennung...

--- src/tb_base/scripts/cvtest.py
+++ src/tb_base/scripts/cvtest.py
@@ -2,6 +2,7 @@
 import sys
 import rospy
 import cv2
+import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -11,9 +12,8 @@ class templateMatcher:
 	def __init__(self):
 		self.bridge = CvBridge()
 		self.template = cv2.imread('rot.png',1)
-		self.lower_color = np.array([50,0,0], dtype=np.uint8)
-		self.upper_color = np.array([255,0,0], dtype=np.uint8)
-
+		self.lower_color = np.array([160,50,100], dtype=np.uint8)
+		self.upper_color = np.array([165,255,200], dtype=np.uint8)
 		self.image_sub = rospy.Subscriber("camera/rgb/image_color",Image,self.callback)
 		self.detected = False
 
@@ -22,6 +22,8 @@ class templateMatcher:
 			img = self.bridge.imgmsg_to_cv2(data, "bgr8")
 		except CvBridgeError as e:
 			print(e)
+		width,height = img.shape[:2]
+		img = img[height/2:height,0:width]
 		res = cv2.matchTemplate(img, self.template, cv2.TM_CCORR_NORMED)
 		min_val,max_val,min_loc,max_loc = cv2.minMaxLoc(res)
 		print min_val, max_val
@@ -32,10 +34,9 @@ class templateMatcher:
 		print self.detected
 		tl = max_loc
 		br = (tl[0] + 100, tl[1]+100)
-		mask = cv2.inRange(hsv, self.lower_color, self.upper_color)
-		cv2.imshow('mask',mask)
 		cv2.rectangle(img,tl,br,255,2)
-		cv2.imshow('test',img)
+		cv2.imshow('mask',img)
+		#cv2.imshow('test',img)
 		cv2.waitKey(5)
 
 def main(args):
