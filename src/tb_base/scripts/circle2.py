9da4ff1e6ec43d3d52ce21759b2a9932caef9f6a
Daniel Kaiser <marx@th-bingen.de>
2017-06-20 13:13:42 +0200

    before range detect test

--- src/tb_base/scripts/circle2.py
+++ src/tb_base/scripts/circle2.py
@@ -16,27 +16,23 @@ class test:
 		self.bridge = CvBridge()
 		#self.template = cv2.imread('kreuz.png',1)
 		# define the lower and upper boundaries of the "green"
-		self.greenLower = (29, 86, 6)
-		self.greenUpper = (64, 255, 255)
-		self.lower_color = np.array([50,200,10], dtype=np.uint8)
-		self.upper_color = np.array([70,255,120], dtype=np.uint8)
+		self.greenLower = (38, 70, 10)
+		self.greenUpper = (95, 255, 96)
+		self.redLower = (123,6,0)
+		self.redUpper = (181,70,200)
 		self.image_sub = rospy.Subscriber("camera/rgb/image_color",Image,self.callback)
 		#self.image_sub = rospy.Subscriber("camera/rgb/image_mono",Image,self.callback)
 
 	def callback(self,data):
 		try:
-			imgC = self.bridge.imgmsg_to_cv2(data, "rgb8")
+			imgC = self.bridge.imgmsg_to_cv2(data, "bgr8")
 		except CvBridgeError as e:
 			print(e)
 		hsv = cv2.cvtColor(imgC, cv2.COLOR_BGR2HSV)
-		# construct a mask for the color "green", then perform
-		# a series of dilations and erosions to remove any small
-		# blobs left in the mask
-		mask = cv2.inRange(hsv, self.greenLower, self.greenUpper)
+		mask = cv2.inRange(hsv, self.redLower, self.redUpper)
+		#mask = cv2.inRange(hsv, self.greenLower, self.greenUpper)
 		mask = cv2.erode(mask, None, iterations=2)
 		mask = cv2.dilate(mask, None, iterations=2)
-		# find contours in the mask and initialize the current
-		# (x, y) center of the ball
 		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
 		center = None
 		# only proceed if at least one contour was found
@@ -49,13 +45,11 @@ class test:
 			M = cv2.moments(c)
 			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 			# only proceed if the radius meets a minimum size
-			print radius
 			if radius > 100 and radius < 150:
-				# draw the circle and centroid on the frame,
-				# then update the list of tracked points
 				cv2.circle(imgC, (int(x), int(y)), int(radius),(0, 255, 255), 2)
 				cv2.circle(imgC, center, 5, (0, 0, 255), -1)
 		cv2.imshow('detected circles',imgC)
+		cv2.imshow('mask',mask)
 		cv2.waitKey(5)
 
 if __name__ == '__main__':
