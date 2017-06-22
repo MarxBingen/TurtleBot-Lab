82abad12bd1d82c4c8e8149acf16d94ada005daf
Daniel Kaiser <marx@th-bingen.de>
2017-06-22 11:36:55 +0200

    Range Detect funzt nun

--- src/tb_base/scripts/range_detect.py
+++ src/tb_base/scripts/range_detect.py
@@ -6,17 +6,18 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from operator import xor
 
-class range_detect:
 
+
+class range_detect:
 	def callback(self,value):
 		pass
 
 	def setup_trackbars(self,range_filter):
-		cv2.namedWindow("Trackbars", 0)
+		cv2.namedWindow("Trackbars",cv2.WINDOW_NORMAL)
 		for i in ["MIN", "MAX"]:
 			v = 0 if i == "MIN" else 255
 			for j in range_filter:
-				cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, self.callback)
+				cv2.createTrackbar("%s_%s" % (j, i), 'Trackbars', v, 255, self.callback)
 
 	def get_arguments(self):
 		ap = argparse.ArgumentParser()
@@ -41,10 +42,10 @@ class range_detect:
 
 	def __init__(self):
 		self.args = self.get_arguments()
-		self.bridge = CvBridge()
-		self.image_sub = rospy.Subscriber("camera/rgb/image_color",Image,self.imCallback)
 		self.range_filter = self.args['filter'].upper()
 		self.setup_trackbars(self.range_filter)
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("camera/rgb/image_color",Image,self.imCallback)
 
 
 	def imCallback(self,data):
@@ -63,18 +64,16 @@ class range_detect:
 			preview = cv2.bitwise_and(image, image, mask=thresh)
 			cv2.imshow("Preview", preview)
 		else:
-			print "hhuuuhuhu"
-			cv2.imshow("Original", image)
-			cv2.waitKey(5)
-			#cv2.imshow("Thresh", thresh)
-		print "Callback-Ende"
-		#cv2.waitKey(0)
+			#cv2.imshow("Original", image)
+			cv2.imshow("Thresh", thresh)
+		cv2.waitKey(5)
 
 if __name__ == '__main__':
 	rospy.init_node('range_detector', anonymous=True)
-	m = range_detect()
-	try:
-		rospy.spin()
-	except:
-		print "Ende"
-	cv2.destroyAllWindows
+	print "press ESC in Trackbar Window to exit"
+	x = range_detect()
+	while not rospy.is_shutdown():
+		k = cv2.waitKey(0)
+		if k == 27:
+			cv2.destroyAllWindows
+			break
