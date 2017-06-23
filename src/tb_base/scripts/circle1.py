900a0e319e303fa3db6e36264c93a3bf949b40b4
Daniel Kaiser <marx@th-bingen.de>
2017-06-23 11:56:06 +0200

    circle1 erkennt nun rot und green

--- src/tb_base/scripts/circle1.py
+++ src/tb_base/scripts/circle1.py
@@ -14,31 +14,57 @@ from cv_bridge import CvBridge, CvBridgeError
 class test:
 	def __init__(self):
 		self.bridge = CvBridge()
-		#self.template = cv2.imread('kreuz.png',1)
-		self.lower_color = np.array([50,200,10], dtype=np.uint8)
-		self.upper_color = np.array([70,255,120], dtype=np.uint8)
+		#Low und High sind in R G B !
+		self.redLow = np.array([47,0,0], dtype=np.uint8)
+		self.redHigh = np.array([255,16,14], dtype=np.uint8)
+		self.greenLow = np.array([0,0,0], dtype=np.uint)
+		self.greenHigh = np.array([22,255,42], dtype=np.uint)
+
 		self.image_sub = rospy.Subscriber("camera/rgb/image_color",Image,self.callback)
 		#self.image_sub = rospy.Subscriber("camera/rgb/image_mono",Image,self.callback)
 
 	def callback(self,data):
 		try:
-			imgC = self.bridge.imgmsg_to_cv2(data, "rgb8")
+			imgO = self.bridge.imgmsg_to_cv2(data, "bgr8")
 		except CvBridgeError as e:
 			print(e)
-		gray = cv2.cvtColor(imgC, cv2.COLOR_RGB2GRAY)
-		#zum filtern nach farbe nach HSV konvertieren
-		#hsv = cv2.cvtColor(imgC,cv2.COLOR_RGB2HSV)
-		#mask = cv2.medianBlur(hsv,5)
-		#mask = cv2.inRange(mask,self.lower_color,self.upper_color)
-		#img = cv2.cvtColor(mask,cv2.COLOR_HSV2RGB)
-		#img = cv2.cvtColor(mask,cv2.COLOR_RGB2GRAY)
-		circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,10,param1=40,param2=50,minRadius=80,maxRadius=160)
-		if (circles is not None):
-			circles = np.uint16(np.around(circles))
-			for i in circles[0,:]:
-				cv2.circle(gray,(i[0],i[1]),i[2],(0,255,0),2)
-		cv2.imshow('detected circles',gray)
-		cv2.waitKey(5)
+		imgC = cv2.cvtColor(imgO, cv2.COLOR_BGR2RGB)
+		imgC = cv2.erode(imgC, None, iterations=2)
+		imgC = cv2.dilate(imgC, None, iterations=2)
+		#red green masking
+		maskRed = cv2.inRange(imgC,self.redLow,self.redHigh)
+		maskGreen = cv2.inRange(imgC,self.greenLow,self.greenHigh)
+		cntsGreen = cv2.findContours(maskGreen, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+		cntsRed = cv2.findContours(maskRed, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+		center = None
+		# only proceed if at least one contour was found
+		if len(cntsGreen) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cntsGreen, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			#M = cv2.moments(c)
+			#center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			# only proceed if the radius meets a minimum size
+			if radius > 100 and radius < 150:
+				cv2.circle(imgO, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+				#cv2.circle(imgO, center, 5, (0, 0, 255), -1)
+		# only proceed if at least one contour was found
+		if len(cntsRed) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cntsRed, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			# only proceed if the radius meets a minimum size
+			if radius > 100 and radius < 150:
+				cv2.circle(imgO, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+				cv2.circle(imgO, center, 5, (0, 0, 255), -1)
+		cv2.imshow('detected circles',imgO)
+		cv2.waitKey(1)
 
 if __name__ == '__main__':
 	rospy.init_node('templateMatcher', anonymous=True)
